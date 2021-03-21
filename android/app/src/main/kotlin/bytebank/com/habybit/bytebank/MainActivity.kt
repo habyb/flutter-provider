8463fcaa845597d7a066deb8f87868417579b3c9
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-21 10:20:51 -0300

    first commit

---
+++ android/app/src/main/kotlin/bytebank/com/habybit/bytebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutterprovider.com.habybit.flutterprovider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutterprovider/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlutterProvider());
+
+class FlutterProvider extends StatelessWidget {
+  const FlutterProvider({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
